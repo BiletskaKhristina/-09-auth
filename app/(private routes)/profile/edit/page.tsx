2b15e0a35f54e366c1b0139c03838b618307df42
Білецька <khrista.biletcka7@gmail.com>
2026-07-30 14:00:56 +0300

    add next image config

---
+++ app/(private routes)/profile/edit/page.tsx
@@ -0,0 +1,151 @@
+ "use client";
+
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+import { updateMe } from "@/lib/api/clientApi";
+import { useAuthStore } from "@/lib/store/authStore";
+
+
+import css from "./EditProfile.module.css";
+
+
+export default function EditProfilePage(){
+
+
+const router = useRouter();
+
+
+const {
+ user,
+ setUser
+}=useAuthStore();
+
+
+const [username,setUsername]=useState(
+ user?.username ?? ""
+);
+
+
+
+if(!user){
+ return null;
+}
+
+
+
+const handleSubmit = async(
+e:React.FormEvent
+)=>{
+
+e.preventDefault();
+
+
+const updatedUser = await updateMe({
+ username
+});
+
+
+setUser(updatedUser);
+
+
+router.push("/profile");
+
+};
+
+
+
+return (
+
+<main className={css.mainContent}>
+
+
+<div className={css.profileCard}>
+
+
+<h1 className={css.formTitle}>
+Edit Profile
+</h1>
+
+
+<Image
+src={user.avatar}
+alt="User Avatar"
+width={120}
+height={120}
+className={css.avatar}
+/>
+
+
+
+<form
+className={css.profileInfo}
+onSubmit={handleSubmit}
+>
+
+
+<div className={css.usernameWrapper}>
+
+
+<label htmlFor="username">
+Username:
+</label>
+
+
+<input
+id="username"
+type="text"
+value={username}
+onChange={(e)=>setUsername(e.target.value)}
+className={css.input}
+/>
+
+
+</div>
+
+
+
+<p>
+Email: {user.email}
+</p>
+
+
+
+<div className={css.actions}>
+
+
+<button
+type="submit"
+className={css.saveButton}
+>
+Save
+</button>
+
+
+
+<button
+type="button"
+className={css.cancelButton}
+onClick={()=>router.push("/profile")}
+>
+Cancel
+</button>
+
+
+</div>
+
+
+</form>
+
+
+</div>
+
+
+</main>
+
+);
+
+}
--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,6 +1,7 @@
 import { cookies } from "next/headers";
 import { api } from "./api";
 import type { Note } from "@/types/note";
+import type { User } from "@/types/user";
 
 
 export const fetchNotes = async () => {
@@ -20,6 +21,19 @@ export const fetchNoteById = async (id: string) => {
   const cookieStore = await cookies();
 
   const { data } = await api.get<Note>(`/notes/${id}`, {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+  });
+
+  return data;
+};
+
+
+export const getMe = async () => {
+  const cookieStore = await cookies();
+
+  const { data } = await api.get<User>("/users/me", {
     headers: {
       Cookie: cookieStore.toString(),
     },
